78eb1633150a288f4dc6b31c2458bb27b7f12ceb
myChinesePYL <3223285598@qq.com>
2024-12-14 22:57:58 +0800

    移除glm残留

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <cgc/cgc.hpp>
+#include <cgc/core/Object3D.hpp>
 #include <iostream>
 
 int main(void) {
+  cgc::core::o3d();
   std::cout << "我的C plus plus学习仓库（简称练手）" << std::endl << cgc::test() << std::endl;
   return 0;
 }

a16af6b48430da9684437efefa6653ee977cc5ee
myChinesePYL <3223285598@qq.com>
2024-12-14 17:13:15 +0800

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
+#include <cgc/cgc.hpp>
 #include <iostream>
 
 int main(void) {
-  std::cout << "我的C plus plus学习仓库（简称练手）" << std::endl;
+  std::cout << "我的C plus plus学习仓库（简称练手）" << std::endl << cgc::test() << std::endl;
   return 0;
 }
